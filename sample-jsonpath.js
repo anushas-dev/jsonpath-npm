1aa6d740a62f89924eb43e97c940ef161458dec6
Anusha S <anushasridharan7@gmail.com>
2022-09-20 09:41:53 +0530

    first commit

---
+++ sample-jsonpath.js
@@ -0,0 +1,34 @@
+var colors = [
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+]
+
+var jp = require('jsonpath');
+var query_result = jp.query(colors, '$..[?(@.color=="magenta")].value');
+console.log(query_result)
